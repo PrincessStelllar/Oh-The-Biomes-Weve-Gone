8053bccd29528b4342b06093f0f333cfccc24e70
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-06-26 14:32:57 -0400

    Remove model stuff

--- Fabric/src/main/java/net/potionstudios/biomeswevegone/fabric/client/BiomesWeveGoneClientFabric.java
+++ Fabric/src/main/java/net/potionstudios/biomeswevegone/fabric/client/BiomesWeveGoneClientFabric.java
@@ -11,7 +11,6 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.color.item.ItemTintSources;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
-import net.potionstudios.biomeswevegone.BiomesWeveGone;
 import net.potionstudios.biomeswevegone.client.BiomesWeveGoneClient;
 
 /**
@@ -37,6 +36,6 @@ public class BiomesWeveGoneClientFabric implements ClientModInitializer, ModelLo
 
     @Override
     public void initialize(Context context) {
-        BiomesWeveGoneClient.registerAdditionalModels(modelLocation -> context.addModels(BiomesWeveGone.id("block/" + modelLocation)));
+
     }
 }
